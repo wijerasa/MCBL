3a3e02cce7af6f8fa9ea73c659a7d1615e9f17ab
Saranga Wijeratne <wijera81@gmail.com>
2015-09-24 15:26:42 -0400

    8  commit

--- conf.py
+++ conf.py
@@ -29,10 +29,10 @@ import alabaster
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = [
-#    'sphinx.ext.autodoc',
-#]
-extensions = ['alabaster']
+extensions = [
+    'sphinx.ext.autodoc',
+]
+#extensions = ['alabaster']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
