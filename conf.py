0f89626a0e3c469262cf6d66fee24e956c63b352
Saranga Wijeratne <wijera81@gmail.com>
2015-09-24 14:35:08 -0400

    8  commit

--- conf.py
+++ conf.py
@@ -107,7 +107,7 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 
-  html_theme_options = {
+html_theme_options = {
     "rightsidebar": "true",
     "relbarbgcolor": "black",
     "collapsiblesidebar" : "true"
