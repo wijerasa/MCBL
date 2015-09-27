1494f970e85927b8e7e590d7e8526e89381cbb53
wijerasa <wijera81@gmail.com>
2015-09-26 20:30:44 -0400

    commit

--- conf.py
+++ conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import alabaster
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -102,7 +103,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster'
-html_theme = 'Default'
+html_theme = 'sphinx_rtd_theme'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
