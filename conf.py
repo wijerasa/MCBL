127385982b202240ccb042219d9c49ba01a4b8de
Saranga Wijeratne <wijera81@gmail.com>
2015-10-22 16:30:48 -0400

    8  commit

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 import alabaster
-import sphinx_rtd_theme
+import sphinx_rtd_theme, mkdocs
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
